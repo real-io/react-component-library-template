62ad53f6d702ac87a21519d0629ed844a370c16c
Raul Argueta <66976499+real-io@users.noreply.github.com>
2023-09-29 13:09:13 -0600

    Updating sample Button component styles

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,12 @@
 import React from "react";
+import styles from './Button.module.scss';
 
 interface IButtonProps {
   text: string;
 }
 
 const Button = (props: IButtonProps) => {
-  return <button>{props.text}</button>;
+  return <button className={styles.button}>{props.text}</button>;
 };
 
 export default Button;
