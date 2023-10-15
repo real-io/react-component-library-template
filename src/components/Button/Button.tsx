b9a8ff4464f205b2bfc307ac6e878c96ad53652c
Raul Argueta <66976499+real-io@users.noreply.github.com>
2023-10-15 02:50:52 -0600

    Update

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,14 +3,22 @@ import styles from "./Button.module.scss";
 import { Button } from "@radix-ui/themes";
 
 interface IButtonProps {
+  addClassName?: string;
   text: string;
   variant: "classic" | "solid" | "soft" | "surface" | "outline" | "ghost";
+  [x: string]: any;
 }
 
-const ButtonWrapper = (props: IButtonProps) => {
+const ButtonWrapper = ({ variant, addClassName, ...props }: IButtonProps) => {
   return (
     <>
-      <Button variant={props.variant} className={styles.button}>{props.text}</Button>
+      <Button
+        variant={variant}
+        className={[styles.button, addClassName].join(" ")}
+        {...props}
+      >
+        {props.text}
+      </Button>
     </>
   );
 };
