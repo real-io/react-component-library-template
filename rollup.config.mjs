13f0e30f2185ff4facebe3ebcaa0a24462049b3a
Raul Argueta <66976499+real-io@users.noreply.github.com>
2023-10-14 23:51:59 -0600

    Updating rollup config

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -27,6 +27,8 @@ export default [
       typescript({ tsconfig: "./tsconfig.json", exclude: ["./src/stories/*", "./**/*stories.(js|jsx|mjs|ts|tsx)"]  }),
       postcss({
         modules: true,
+        extract: 'main.css',
+        sourceMap: true,
       }),
     ],
   },
